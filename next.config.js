a0a48bd3a4a5d7cc554dd6baf1c7ccc2da1139f9
Engineerbabu777 <awaismumtaz0099@gmail.com>
2024-01-07 17:14:56 +0500

    'added'

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
 
-module.exports = nextConfig
+    images: {
+      domains: [
+        'res.cloudinary.com', 
+        'avatars.githubusercontent.com',
+        'lh3.googleusercontent.com'
+      ]
+    }
+  }
+  
+  module.exports = nextConfig
